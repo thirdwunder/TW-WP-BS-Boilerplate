5b4422602551163ba5502d0a9dcef0e08acb52e4
mohamedhamad <mohamed@mohamed-hamad.com>
2015-01-19 23:53:45 -0500

    moving scroll affix to theme js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,3 +2,14 @@
 // @codekit-prepend "../../includes/jasny/dist/js/jasny-bootstrap.js";
 // @codekit-prepend "../../includes/isotope/dist/isotope.pkgd.js";
 // @codekit-prepend "../../includes/tw-wp-core/js/tw_script.js";
+
+/*
+jQuery('#scroll-to-top-wrapper').affix({
+  offset: {
+    top: 100,
+    bottom: function () {
+      return (this.bottom = jQuery('#site-footer').outerHeight(true));
+    }
+  }
+});
+*/
