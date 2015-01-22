44f0d419f03df613eb3ec7b2358378028266e662
mohamedhamad <mohamed@mohamed-hamad.com>
2015-01-22 11:42:05 -0500

    new jumbotron

--- template-homepage.php
+++ template-homepage.php
@@ -56,40 +56,43 @@ get_header(); ?>
 <!-- Site Container -->
 <div id="site-content" <?php post_class('container-fluid');?> role="main" itemprop="mainContentOfPage">
   <?php if($j_enabled): ?>
-  <div class="row jumbotron <?php echo $j_color; ?>" style="<?php echo $j_bg_style; ?>">
-    <?php if($j_video_url==''): ?>
-      <div class="col-xs-12 col-sm-12 col-md-12">
-        <?php if(isset($j_options['tw_jumbotron_title'])):?><h1><?php echo $j_options['tw_jumbotron_title'];?></h1><?php endif; ?>
-        <?php if(isset($j_options['tw_jumbotron_text'])):?><p><?php echo $j_options['tw_jumbotron_text'];?></p><?php endif; ?>
-        <?php if($j_button  && $j_url): ?>
-        <p><a class="btn btn-primary btn-lg" href="<?php echo $j_url;?>" role="button"><?php echo $j_button;?></a></p>
-        <?php endif; ?>
-      </div>
-    <?php else: ?>
-      <div class="col-xs-12 col-sm-12 col-md-6">
-
-        <?php if(isset($j_options['tw_jumbotron_title'])):?><h1><?php echo $j_options['tw_jumbotron_title'];?></h1><?php endif; ?>
-        <?php if(isset($j_options['tw_jumbotron_text'])):?><p><?php echo $j_options['tw_jumbotron_text'];?></p><?php endif; ?>
-        <?php if(isset($j_options['tw_jumbotron_button_title'])  && isset($j_options['tw_jumbotron_button_url'])): ?>
-        <p><a class="btn btn-primary btn-lg" href="<?php echo $j_options['tw_jumbotron_button_url'];?>" role="button"><?php echo $j_options['tw_jumbotron_button_title'];?></a></p>
-        <?php endif; ?>
-
-      </div>
-      <div class="col-xs-12 col-sm-12 col-md-6">
-        <?php if($j_video_poster_img!==''): ?>
-          <div id="section-video-<?php echo get_the_id();?>" class="section-video" style="<?php echo $j_video_poster_style; ?>">
-            <div id="video-<?php echo get_the_id();?>" class="video embed-responsive embed-responsive-16by9" >
-              <!-- <?php echo html_entity_decode($j_video_embed); ?> -->
-            </div>
+  <div class="jumbotron-wrapper row">
+  <div class="jumbotron <?php echo $j_color; ?>" style="<?php echo $j_bg_style; ?>">
+    <div class="jumbotron-container">
+        <?php if($j_video_url==''): ?>
+          <div class="jumbotron-content no-video col-xs-12 col-sm-12 col-md-12">
+            <?php if(isset($j_options['tw_jumbotron_title'])):?><h1><?php echo $j_options['tw_jumbotron_title'];?></h1><?php endif; ?>
+            <?php if(isset($j_options['tw_jumbotron_text'])):?><p><?php echo $j_options['tw_jumbotron_text'];?></p><?php endif; ?>
+            <?php if($j_button  && $j_url): ?>
+            <p><a class="btn btn-primary btn-lg" href="<?php echo $j_url;?>" role="button"><?php echo $j_button;?></a></p>
+            <?php endif; ?>
           </div>
         <?php else: ?>
-          <div class="video embed-responsive embed-responsive-16by9" >
-            <?php echo html_entity_decode($j_video_embed); ?>
+          <div class="jumbotron-content has-video col-xs-12 col-sm-12 col-md-6">
+
+            <?php if(isset($j_options['tw_jumbotron_title'])):?><h1><?php echo $j_options['tw_jumbotron_title'];?></h1><?php endif; ?>
+            <?php if(isset($j_options['tw_jumbotron_text'])):?><p><?php echo $j_options['tw_jumbotron_text'];?></p><?php endif; ?>
+            <?php if(isset($j_options['tw_jumbotron_button_title'])  && isset($j_options['tw_jumbotron_button_url'])): ?>
+            <p><a class="btn btn-primary btn-lg" href="<?php echo $j_options['tw_jumbotron_button_url'];?>" role="button"><?php echo $j_options['tw_jumbotron_button_title'];?></a></p>
+            <?php endif; ?>
+
+          </div>
+          <div class="jumbotron-video col-xs-12 col-sm-12 col-md-6">
+            <?php if($j_video_poster_img!==''): ?>
+              <div id="section-video-<?php echo get_the_id();?>" class="section-video" style="<?php echo $j_video_poster_style; ?>">
+                <div id="video-<?php echo get_the_id();?>" class="video embed-responsive embed-responsive-16by9" >
+                  <!-- <?php echo html_entity_decode($j_video_embed); ?> -->
+                </div>
+              </div>
+            <?php else: ?>
+              <div class="video embed-responsive embed-responsive-16by9" >
+                <?php echo html_entity_decode($j_video_embed); ?>
+              </div>
+            <?php endif; ?>
           </div>
         <?php endif; ?>
-      </div>
-    <?php endif; ?>
-
+    </div><!-- jumbotron-container -->
+  </div><!-- jumbotron -->
   </div>
   <?php endif; ?>
 
