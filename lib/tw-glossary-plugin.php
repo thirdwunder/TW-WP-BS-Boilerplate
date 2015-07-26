18652efbe73df73c6e4a5daf309be118c4a6d01b
mohamedhamad <mohamed@thirdwunder.com>
2015-07-26 11:27:36 -0400

    glossary css

--- lib/tw-glossary-plugin.php
+++ lib/tw-glossary-plugin.php
@@ -22,8 +22,3 @@ function tw_glossary_plugin_before_glossary_action(){
 add_action('tw_glossary_plugin_before_glossary_single', 'tw_glossary_plugin_before_glossary_action', 10);
 add_action('tw_glossary_plugin_before_glossary_archive', 'tw_glossary_plugin_before_glossary_action', 10);
 add_action('tw_glossary_plugin_before_glossary_template', 'tw_glossary_plugin_before_glossary_action', 10);
-
-function tw_glossary_plugin_before_glossary_title_action(){
-  //get_template_part( 'content/_breadcrumbs' );
-}
-add_action('tw_glossary_plugin_before_glossary_single_title', 'tw_glossary_plugin_before_glossary_title_action', 10);
