4e7e9bf3aa568410d920f9c539e46a9f78cd93e8
mohamedhamad <mohamed@thirdwunder.com>
2015-07-26 11:36:04 -0400

    breadcrumbs row

--- content/_breadcrumbs.php
+++ content/_breadcrumbs.php
@@ -1,5 +1,5 @@
 <?php if ( function_exists('yoast_breadcrumb') && !is_front_page()  ) {
-  $breadcrumbs = yoast_breadcrumb( '<ul class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList"><li itemprop="itemListElement" itemscope
+  $breadcrumbs = yoast_breadcrumb( '<ul class="breadcrumb row" itemscope itemtype="http://schema.org/BreadcrumbList"><li itemprop="itemListElement" itemscope
       itemtype="http://schema.org/ListItem">', '</li></ul>', false );
   $breadcrumbs = str_replace('Home', '<i class="fa fa-home"></i>', $breadcrumbs);
   $breadcrumbs = str_replace( '&raquo;', '</li><li>', $breadcrumbs );
