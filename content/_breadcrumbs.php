e9c8d168cd097ed3e866bff49216bd800d089d42
mohamedhamad <mohamed@thirdwunder.com>
2015-07-26 11:44:52 -0400

    breadcrumb wrapper

--- content/_breadcrumbs.php
+++ content/_breadcrumbs.php
@@ -1,8 +1,14 @@
-<?php if ( function_exists('yoast_breadcrumb') && !is_front_page()  ) {
-  $breadcrumbs = yoast_breadcrumb( '<ul class="breadcrumb row" itemscope itemtype="http://schema.org/BreadcrumbList"><li itemprop="itemListElement" itemscope
+<?php if ( function_exists('yoast_breadcrumb') && !is_front_page()  ): ?>
+<div class="breadcrumb-wrapper row">
+  <div class="container">
+<?php
+  $breadcrumbs = yoast_breadcrumb( '<ul class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList"><li itemprop="itemListElement" itemscope
       itemtype="http://schema.org/ListItem">', '</li></ul>', false );
   $breadcrumbs = str_replace('Home', '<i class="fa fa-home"></i>', $breadcrumbs);
   $breadcrumbs = str_replace( '&raquo;', '</li><li>', $breadcrumbs );
   $breadcrumbs = str_replace( '<a', '<a itemprop="item"', $breadcrumbs );
   echo $breadcrumbs;
-} ?>
+?>
+  </div>
+</div>
+<?php endif;?>
